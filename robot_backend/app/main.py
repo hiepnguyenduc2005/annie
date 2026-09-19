6e1bd57254ca00cef14109c4ec069fc74502b847
hiepnguyenduc2005 <hiepnguyenduc2005@gmail.com>
2026-09-19 17:42:10 -0400

    clean

--- robot_backend/app/main.py
+++ robot_backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from .api.router import router
+from app.api.router import router
 
 app = FastAPI(title="Annie Robot API", version="0.1.0")
 app.include_router(router)
